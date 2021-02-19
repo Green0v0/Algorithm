bce1e61912d4112c1a5f692f1723225ab4d26131
ij_slango <junginje@gmail.com>
2021-02-19 21:27:20 +0900

    210219 디렉토리 update

--- slango/This Is CoTe/Practice/03. DFS_BFS/17. 경쟁적 전염.py
+++ slango/This Is CoTe/Practice/03. DFS_BFS/17. 경쟁적 전염.py
@@ -3,7 +3,7 @@ from collections import deque
 # 입력받기
 n, k = map(int, input().split())
 
-maps = [[]]
+maps = [[0]*(n+1)]
 for _ in range(n):
     maps.append([0] + list(map(int, input().split())))
 
@@ -14,7 +14,7 @@ s, x, y = map(int, input().split())
 virs_idx = [[] for _ in range(k+1)]
 for i in maps:
     for j in i:
-        # 해당 자리에 바이러스가 있다면 좌표 virs_idx에 넣기
+        # 해당 자리에 바이러스가 있다면 좌표 virs_idx에 넣기awwwwwwwwwwwww
         if j:
             virs_idx[j] = [maps.index(i), i.index(j)]
 
@@ -29,30 +29,35 @@ for i in virs_idx:
 
 # bfs 메소드 정의
 def vir_bfs(now_vir, maps):
-    v = virs_que[now_vir].popleft()
-    if v[0] > 1 and not maps[v[0]-1][v[1]]:
-        virs_que[now_vir].append([v[0]-1, v[1]])
-        maps[v[0]-1][v[1]] = now_vir
-    if v[0] < n and not maps[v[0]+1][v[1]]:
-        virs_que[now_vir].append([v[0]+1, v[1]])
-        maps[v[0]+1][v[1]] = now_vir
-    if v[1] > 1 and not maps[v[0]][v[1]-1]:
-        virs_que[now_vir].append([v[0], v[1]-1])
-        maps[v[0]][v[1]-1] = now_vir
-    if v[1] < n and not maps[v[0]][v[1]+1]:
-        virs_que[now_vir].append([v[0], v[1]+1])
-        maps[v[0]][v[1]+1] = now_vir
+    # 한번에 푸쉬할 임시큐
+    tmp_q = virs_que[now_vir]
+    virs_que[now_vir] = deque()
+
+    for v in tmp_q:
+        if v[0] > 1 and not maps[v[0]-1][v[1]]:
+            virs_que[now_vir].append([v[0]-1, v[1]])
+            maps[v[0]-1][v[1]] = now_vir
+        if v[0] < n and not maps[v[0]+1][v[1]]:
+            virs_que[now_vir].append([v[0]+1, v[1]])
+            maps[v[0]+1][v[1]] = now_vir
+        if v[1] > 1 and not maps[v[0]][v[1]-1]:
+            virs_que[now_vir].append([v[0], v[1]-1])
+            maps[v[0]][v[1]-1] = now_vir
+        if v[1] < n and not maps[v[0]][v[1]+1]:
+            virs_que[now_vir].append([v[0], v[1]+1])
+            maps[v[0]][v[1]+1] = now_vir
+
 
 for _ in range(s):
-    for i in range(k):
-        vir_bfs(i+1, maps)
+    for i in range(1, k+1):
+        vir_bfs(i, maps)
 
 print(maps[x][y])
 
 
 # # 1st try:
 # from collections import deque
-#
+#w
 # # 입력받기
 # n, k = map(int, input().split())
 #
@@ -120,3 +125,8 @@ print(maps[x][y])
 # 0 0 0 0
 # 0 0 0 2
 # 3 3 2
+
+# 2 4
+# 1 2
+# 3 4
+# 1 1 1
