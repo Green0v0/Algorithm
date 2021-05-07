742d5b64963581220eee996035ad1a676c3497b7
Green0v0 <greenwint3r@gmail.com>
2021-05-07 23:53:02 +0900

    add code

--- Green/notion/2019카카오겨울인턴_크레인 인형뽑기 게임.py
+++
@@ -1,6 +0,0 @@
-# 터트려져 사라진 인형의 개수를 return
-def solution(board, moves):
-    answer = 0
-    return answer
-
-solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]],[1,5,3,5,1,2,1,4])
