8a969365be4648b753a1c09ddff8902055650c95
Green0v0 <greenwint3r@gmail.com>
2021-05-07 23:54:28 +0900

    add code

--- Green/Sort/국영수.py
+++ Green/Sort/국영수.py
@@ -24,17 +24,17 @@ for _ in range(N):
 
 # 국어 영어 수학
 student.sort(key=lambda x:(-x[1], x[2], -x[3], x[0]))
-for n, k, e, m, ascii_cord in student:
+for n, k, e, m in student:
     print(n)
 
 # 다른 사람 방법
-N = int(input())
-score_list = []
-for i in range(N):
-    [name, kor, eng, math] = map(str, input().split())
-    score_list.append([name, int(kor), int(eng), int(math)])
-
-sorted_score_list = sorted(score_list, key=lambda x : (-x[1], x[2], -x[3], x[0]))
-
-for score in sorted_score_list:
-    print(score[0])
+# N = int(input())
+# score_list = []
+# for i in range(N):
+#     [name, kor, eng, math] = map(str, input().split())
+#     score_list.append([name, int(kor), int(eng), int(math)])
+#
+# sorted_score_list = sorted(score_list, key=lambda x : (-x[1], x[2], -x[3], x[0]))
+#
+# for score in sorted_score_list:
+#     print(score[0])
